2a1201d32e4a81c219297d7de9db6c6acbd18a72
Erik Bjørnsen <bjornsen.erik@gmail.com>
2024-02-08 13:15:04 +0100

    init commit

---
+++ src/main/java/no/ntnu/idatg2003/chaosgame/ChaosgameApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatg2003.chaosgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaosgameApplication.class, args);
+	}
+
+}
