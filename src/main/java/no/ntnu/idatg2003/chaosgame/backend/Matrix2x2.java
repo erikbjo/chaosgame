265f5a483596a8927186872927e801d359b51a29
Eskil Alstad <eskilal@stud.ntnu.no>
2024-02-08 13:27:15 +0100

    added javadoc to matrix class

--- src/main/java/no/ntnu/idatg2003/chaosgame/backend/Matrix2x2.java
+++ src/main/java/no/ntnu/idatg2003/chaosgame/backend/Matrix2x2.java
@@ -1,5 +1,8 @@
 package no.ntnu.idatg2003.chaosgame.backend;
 
+/**
+ * A class representing a 2x2 matrix
+ */
 public class Matrix2x2 {
 
     private double a00;
@@ -8,7 +11,13 @@ public class Matrix2x2 {
     private double a11;
 
 
-
+    /**
+     * Constructor for the Matrix2x2 class
+     * @param a00
+     * @param a01
+     * @param a10
+     * @param a11
+     */
     public Matrix2x2(double a00, double a01, double a10, double a11) {
         this.a00 = a00;
         this.a01 = a01;
@@ -17,7 +26,11 @@ public class Matrix2x2 {
     }
 
 
-
+    /**
+     * Multiplies the matrix with a vector
+     * @param v
+     * @return the resulting vector
+     */
     public Vector2D multiply(Vector2D v) {
         double x = a00 * v.getX() + a01 * v.getY();
         double y = a10 * v.getX() + a11 * v.getY();
